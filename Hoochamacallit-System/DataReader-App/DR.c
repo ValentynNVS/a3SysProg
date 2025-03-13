7a71a349b48c6a8198fd6469a045c2d3863b7ae6
Warren Christopher Bwogi <wbwogi0568@conestogac.on.ca>
2025-03-13 03:38:32 -0400

    Added a function that updates the masterlist and also one that removes inactive machines from the masterlist.

--- Hoochamacallit-System/DataReader-App/DR.c
+++ Hoochamacallit-System/DataReader-App/DR.c
@@ -18,9 +18,22 @@ int main(void)
         sharedMemoryID = createSharedMemory(uniqueToken);
     }
 
+    MasterList *master;
+ 	master = (MasterList *)shmat(sharedMemoryID, NULL, 0); // this allows the dr to attach to the shared memory and begin producing data to be read
+    master->messageQueueID = messageQueueID;
+    master->numberOfMachines = 0;
+
+    logEvent("DR started. Sleeping for 15 seconds before processing messages.");
     sleep(15);
 
+    while (1) {
 
+    }
+
+	msgctl(messageQueueID, IPC_RMID, 0); // clean up the message queue
+    shmdt(master); // detaching from shared memory
+    shmctl(sharedMemoryID, IPC_RMID, 0); // remove the shared memory resource
+    logEvent("Message queue and shared memory removed. DR shutting down.");
     return 0;
 }
 
@@ -43,3 +56,58 @@ int createSharedMemory(key_t uniqueToken)
     }
     return sharedMemoryID;
 }
+
+// This function logs the events happening into a log file
+void logEvent(const char *msg) {
+    FILE *logFile = fopen(LOG_FILE, "a");
+    if (logFile) {
+        time_t now = time(NULL);
+        fprintf(logFile, "[%s] : %s\n", ctime(&now), msg);
+        fclose(logFile);
+    }
+}
+
+// This function updates the masterlist
+void updateMasterList(MasterList *master, QueueMessage *msg) {
+  	time_t currentTime = time(NULL);
+    int found = 0;
+
+    for (int i = 0; i < master->numberOfMachines; i++) {
+        if (master->machines[i].machineID == msg->machineID) {
+            master->machines[i].lastTimeHeardFrom = currentTime;
+            char logEntry[256];
+            snprintf(logEntry, sizeof(logEntry), "DC-%02d [%d] updated in master list – MSG RECEIVED – Status %d (%s)", i + 1, msg->machineID, msg->statusCode, msg->statusDescription);
+            logEvent(logEntry);
+
+            if (msg->statusCode == 6) {  // Status 6 = DC offline
+                snprintf(logEntry, sizeof(logEntry), "DC-%02d [%d] has gone OFFLINE – removing from master list", i + 1, msg->machineID);
+                logEvent(logEntry);
+                removeInactiveMachines(master);
+            }
+            return;
+        }
+    }
+
+    if (master->numberOfMachines < kMaxNumberOfMachines) {
+        master->machines[master->numberOfMachines].machineID = msg->machineID;
+        master->machines[master->numberOfMachines].lastTimeHeardFrom = currentTime;
+        master->numberOfMachines++;
+        char logEntry[256];
+        snprintf(logEntry, sizeof(logEntry), "DC-%02d [%d] added to master list – NEW DC – Status 0 (Everything is OKAY)", master->numberOfMachines, msg->machineID);
+        logEvent(logEntry);
+    }
+}
+
+//This function removes inactive machines from the masterlist
+void removeInactiveMachines(MasterList *master) {
+  	time_t now = time(NULL);
+
+    for (int i = 0; i < master->numberOfMachines; i++) {
+        if (difftime(now, master->machines[i].lastTimeHeardFrom) > 35) {  // 35 sec timeout
+            char logEntry[256];
+            snprintf(logEntry, sizeof(logEntry), "DC-%02d [%d] removed from master list – NON-RESPONSIVE", i + 1, master->machines[i].machineID);
+            logEvent(logEntry);
+        }
+    }
+}
+
--- Hoochamacallit-System/DataReader-App/DR.h
+++ Hoochamacallit-System/DataReader-App/DR.h
@@ -1,4 +1,3 @@
-//
 // This file contains the necessary libraries, constants, prototypes, and structures that the server (DR) needs
 // to function. (i.e. receive messages from machines, keep track of active machines, log key events, etc.)
 //
@@ -9,16 +8,13 @@
 #include <sys/ipc.h> //NOTE: Is this needed??
 #include <sys/shm.h>
 #include <unistd.h>
+#include <time.h>
 #include "../Common/inc/MessageQueue.h"
 
 // Constants
 #define  kMaxNumberOfMachines 10
 #define  kSharedMemorySeed 16535
-
-
-// Function prototypes
-int sharedMemoryExists(key_t uniqueToken);
-int createSharedMemory(key_t uniqueToken);
+#define LOG_FILE "/tmp/dataMonitor.log"
 
 // Structures
 typedef struct MachineInfo
@@ -34,5 +30,11 @@ typedef struct MasterList
     MachineInfo machines[kMaxNumberOfMachines];
 } MasterList;
 
+// Function prototypes
+int sharedMemoryExists(key_t uniqueToken);
+int createSharedMemory(key_t uniqueToken);
+void logEvent(const char *msg);
+void updateMasterList(MasterList *master, QueueMessage *msg);
+void removeInactiveMachines(MasterList *master);
 
 #endif //DR_H
